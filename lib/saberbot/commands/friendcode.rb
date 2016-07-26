e935615221396b3e99d097d5671aa953779f7fd3
Alex Taber <alex@ataber.pw>
2016-07-26 16:38:28 -0400

    Fixed nicks (again)

--- lib/saberbot/commands/friendcode.rb
+++ lib/saberbot/commands/friendcode.rb
@@ -42,8 +42,10 @@ module SaberBot
         max_args: 1
       ) do |event, user|
 
-        user = user.delete! '!'
-
+        if user.include? '!'
+          user = user.delete! '!'
+        end
+        
         if SaberConfig.friendcodes.filter(:user => event.message.author.mention).count == 0
           event.channel.send("You need to register a FC before looking up others!")
           break
