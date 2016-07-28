e2219d3aefbf217a571c4faa4d500b1bf904490e
Megumi Sonoda <sonodamegumi@outlook.com>
2016-07-28 03:44:21 -0600

    Minor cleanup of friend-code stuff

--- lib/saberbot/commands/friendcode.rb
+++ lib/saberbot/commands/friendcode.rb
@@ -10,10 +10,11 @@ module SaberBot
       command(
         :fcregister,
         description: "Register friend code for your user with the bot.\n" \
-                     "Usage: `!fcregister nnnn-nnnn-nnnn`",
+                     'Usage: `!fcregister nnnn-nnnn-nnnn`',
         min_args: 1,
         max_args: 1
       ) do |event, friendcode|
+        break if event.channel.private?
 
         if SaberConfig.friendcodes.filter(:user => event.message.author.mention).count == 0
           if friendcode.gsub('-', '').match('^\d{12}$')
@@ -22,14 +23,14 @@ module SaberBot
               :fc => "#{friendcode}"
             )
 
-            event.channel.send("Successfully registered friendcode!")
+            event.channel.send('Successfully registered friendcode!')
           else
-            event.channel.send("Invalid friendcode!")
+            event.channel.send('Invalid friendcode!')
           end
 
         else
 
-          event.channel.send("Please delete your old FC before adding a new one!")
+          event.channel.send('Please delete your old FC before adding a new one!')
 
         end
       end
@@ -37,45 +38,42 @@ module SaberBot
       command(
         :fcquery,
         description: "Lookup friendcode of a user.\n" \
-                     "Usage: `!fcquery {user}`",
+                     'Usage: `!fcquery <user>`',
         min_args: 1,
         max_args: 1
       ) do |event, user|
-
-        if user.include? '!'
-          user = user.delete! '!'
-        end
+        user = user.delete! '!' if user.include? '!'
 
         if SaberConfig.friendcodes.filter(:user => event.message.author.mention).count == 0
-          event.channel.send("You need to register a FC before looking up others!")
+          event.channel.send('You need to register a FC before looking up others!')
           break
         end
 
         if SaberConfig.friendcodes.filter(:user => user).count == 0
-          event.channel.send("User does not have a friendcode registered!")
+          event.channel.send('User does not have a friendcode registered!')
           break
         end
 
         user_fc = SaberConfig.friendcodes.filter(:user => user)
-        user_fc.each{ |entry| event.channel.send(entry[:fc]) }
+        user_fc.each { |entry| event.channel.send(entry[:fc]) }
 
         author_fc = SaberConfig.friendcodes.filter(:user => event.message.author.mention)
 
         event.message.mentions[0].pm(
           "#{event.message.author.mention} has requested to add your 3ds friend code!\n\n" \
           "Their FC is #{author_fc.first[:fc]}"
-          )
+        )
       end
 
       command(
         :fcdelete,
         description: "Delete the FC associated with your account.\n" \
-                     "Usage: `!fcdelete`",
+                     'Usage: `!fcdelete`',
         max_args: 0
       ) do |event|
+        break if event.channel.private?
 
         SaberConfig.friendcodes.where(:user => event.message.author.mention).delete
-
         'Successfully deleted record!'
       end
     end
