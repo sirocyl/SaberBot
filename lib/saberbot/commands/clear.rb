b79e6272f96b9dbfd26ce5dcf20607589210b933
Megumi Sonoda <sonodamegumi@outlook.com>
2016-07-29 05:21:21 -0600

    Add message clear logging

--- lib/saberbot/commands/clear.rb
+++ lib/saberbot/commands/clear.rb
@@ -16,6 +16,10 @@ module SaberBot
         break if event.channel.private?
         begin
           event.channel.prune(amount.to_i)
+          SaberConfig.server_channels[event.server][SaberConfig.settings['modlog_channel']].send(
+            "**Messages Cleared:** #{amount} in #{event.channel.mention}\n" \
+            "**Responsible Moderator:** #{event.message.author.mention}"
+          )
         rescue ArgumentError
           "Invalid amount \"#{amount}\". Expected a number between 2 and 100."
         end
